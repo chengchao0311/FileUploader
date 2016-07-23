eba1b0f4db96de35c9ccdaecf04f9351607ae720
Shawn Chen <chengchao0311@gmail.com>
2016-07-22 23:28:21 -0400

    USE PUMA

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715074741) do
+ActiveRecord::Schema.define(version: 20160723014832) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
 end
